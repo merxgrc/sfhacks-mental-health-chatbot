97950d8b5fde754a7d34e5bc568d4735c49faa26
merxgrc <mgarcia73@mail.sfsu.edu>
2025-04-04 21:03:51 -0700

    uses pip freeze to add requirements

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def prompt(x):
-    prompt = input("Say something")
+from google import genai
 
+client = genai.Client(api_key="AIzaSyDX47ZTx5qRx2X7D1X3erKup9tzLS0NLGw")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain what a cat is"
+)
+print(response.text)
